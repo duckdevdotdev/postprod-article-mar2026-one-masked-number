843526b5b65d95934196384fccb693a375984f47
misa7067 <106912737+misa7067@users.noreply.github.com>
2026-04-14 19:56:46 +0400

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     EXOLVE_API_KEY = os.getenv("EXOLVE_API_KEY", "")
-    AUTH_POOL_NUMBER = os.getenv("AUTH_POOL_NUMBER", "")
+    SMS_SENDER = os.getenv("SMS_SENDER", "")
     SINGLE_SERVICE_NUMBER = os.getenv("SINGLE_SERVICE_NUMBER", "")
     SUPPORT_NUMBER = os.getenv("SUPPORT_NUMBER", "")
     BITRIX_WEBHOOK = os.getenv("BITRIX_WEBHOOK", "")
